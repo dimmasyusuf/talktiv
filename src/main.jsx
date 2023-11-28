b38ebc3ff5b42470f6b8020c44ffcf71f078e4d7
dimmasyusuf <dimasyusufqurohman@gmail.com>
2023-11-28 21:20:49 +0700

    feat: create auth user action and reducer

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import App from './App.jsx';
-import { store } from './states/store.js';
+import { store } from './states/index.js';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
