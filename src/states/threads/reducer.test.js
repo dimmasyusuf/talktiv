c2fcc316f6dbb46a1b0ef74bdbc643d3f7457ba8
dimmasyusuf <dimasyusufqurohman@gmail.com>
2023-12-08 21:04:39 +0700

    test: create threads reducer unit test

---
+++ src/states/threads/reducer.test.js
@@ -0,0 +1,95 @@
+import { describe, it, expect } from 'vitest';
+import threadsReducer from './reducer';
+
+describe('threadsReducer function', () => {
+  it('should return the initial state when given by unknown action', () => {
+    // arrange
+    const initialState = [];
+    const action = { type: 'UNKNOWN' };
+
+    // action
+    const nextState = threadsReducer(initialState, action);
+
+    // assert
+    expect(nextState).toEqual(initialState);
+  });
+
+  it('should return the threads when given by RECEIVE_THREADS action', () => {
+    // arrange
+    const initialState = [];
+    const action = {
+      type: 'RECEIVE_THREADS',
+      payload: {
+        threads: [
+          {
+            id: 1,
+            title: 'Thread 1',
+            body: 'Body 1',
+            category: 'Category 1',
+            createdAt: '2018-01-01T00:00:00.000Z',
+            ownerId: 'users-1',
+            upVotesBy: [],
+            downVotesBy: [],
+            totalComments: 0,
+          },
+          {
+            id: 2,
+            title: 'Thread 2',
+            body: 'Body 2',
+            category: 'Category 2',
+            createdAt: '2018-01-02T00:00:00.000Z',
+            ownerId: 'users-2',
+            upVotesBy: [],
+            downVotesBy: [],
+            totalComments: 0,
+          },
+        ],
+      },
+    };
+
+    // action
+    const nextState = threadsReducer(initialState, action);
+
+    expect(nextState).toEqual(action.payload.threads);
+  });
+
+  it('should return the threads with the new thread when given by ADD_THREAD action', () => {
+    // arrange
+    const initialState = [
+      {
+        id: 1,
+        title: 'Thread 1',
+        body: 'Body 1',
+        category: 'Category 1',
+        createdAt: '2018-01-01T00:00:00.000Z',
+        ownerId: 'users-1',
+        upVotesBy: [],
+        downVotesBy: [],
+        totalComments: 0,
+      },
+    ];
+
+    const action = {
+      type: 'ADD_THREAD',
+      payload: {
+        thread: {
+          id: 2,
+          title: 'Thread 2',
+          body: 'Body 2',
+          category: 'Category 2',
+          createdAt: '2018-01-02T00:00:00.000Z',
+          ownerId: 'users-2',
+          upVotesBy: [],
+          downVotesBy: [],
+          totalComments: 0,
+        },
+      },
+    };
+
+    // action
+    const nextState = threadsReducer(initialState, action);
+
+    // assert
+    expect(nextState).toEqual([action.payload.threads, ...initialState]);
+  });
+});
